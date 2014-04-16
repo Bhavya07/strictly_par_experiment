66d81fcebc7d2863bd78ec1764d9df83c91cbe3e
Bhavya07 <bhavya.vp7@gmail.com>
2014-04-16 10:54:10 -0700

    Initial devise integration with sign up

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'akashbarik23@gmail.com'
-config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140408081300) do
 
-  create_table "educations", force: true do |t|
-    t.string   "degree"
-    t.string   "Institute"
-    t.string   "Year"
-    t.string   "CGPA"
-    t.string   "Percentage"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
-
   create_table "studies", force: true do |t|
     t.string   "degree"
     t.string   "institution"
@@ -57,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140408081300) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name",                                null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
